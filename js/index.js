7ebf3a6ff502cd2b35d75137a58860e098650435
thirdink <rumitwashere@gmail.com>
2016-03-18 15:58:45 +0545

    made simpleLens work

---
+++ js/index.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+    $('.responsive').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        autoplay:true,
+        autoplaySpeed:4000,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                // centerMode: true,
+
+            }
+
+        }, {
+            breakpoint: 800,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 2,
+                dots: true,
+                infinite: true,
+
+            }
+
+
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                dots: true,
+                infinite: true,
+
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                infinite: true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+            }
+        }]
+    });
+
+
+});
---
+++ php/config.php
@@ -0,0 +1,13 @@
+<?php 
+/* config.php */
+
+/* UPTAKE 2 Responsive Template */
+/* autor: entiri s.r.o. */
+
+/* Enter your e-mail address */
+/* All messages from the website will be sent to this e-mail address. */
+
+$email_address = 'your-email@domain.com';
+                                             
+/* Location: php/config.php */
+?>
